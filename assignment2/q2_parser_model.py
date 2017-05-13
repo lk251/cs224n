6210ede9e5b569c9e29c24b0ea2aec52f3368c44
Lev Kusanagi <levkusanagi@gmail.com>
2017-05-13 18:27:20 +0200

    Create feed dictionary.

--- assignment2/q2_parser_model.py
+++ assignment2/q2_parser_model.py
@@ -83,6 +83,10 @@ class ParserModel(Model):
             feed_dict: The feed dictionary mapping from placeholders to values.
         """
         ### YOUR CODE HERE
+        feed_dict = {self.input_placeholder: inputs_batch}
+        if labels_batch is not None:
+            feed_dict[self.labels_placeholder] = labels_batch
+        feed_dict[self.dropout_placeholder] = dropout
         ### END YOUR CODE
         return feed_dict
 
