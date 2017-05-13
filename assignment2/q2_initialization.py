ccfbfab793968209b84bc8420de8eef2900191bc
Lev Kusanagi <levkusanagi@gmail.com>
2017-05-13 14:37:21 +0200

    Compute square root differently.

--- assignment2/q2_initialization.py
+++ assignment2/q2_initialization.py
@@ -25,7 +25,7 @@ def xavier_weight_init():
             out: tf.Tensor of specified shape sampled from the Xavier distribution.
         """
         ### YOUR CODE HERE
-        epsilon = sqrt(6 / sum(shape))
+        epsilon = (6 / sum(shape))**.5
         out = tf.random_uniform(shape, minval=-epsilon, maxval=epsilon)
         ### END YOUR CODE
         return out
