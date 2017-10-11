d7a31d79006c5b4e834fdf789ef166551585ec39
Lev Kusanagi <levkusanagi@gmail.com>
2017-10-12 01:16:23 +0200

    Initialize U, b2, and h.

--- assignment3/q2_rnn.py
+++ assignment3/q2_rnn.py
@@ -268,6 +268,9 @@ class RNNModel(NERModel):
         # Define U and b2 as variables.
         # Initialize state as vector of zeros.
         ### YOUR CODE HERE (~4-6 lines)
+        U = tf.get_variable("U", shape=(Config.hidden_size, Config.n_classes), initializer=tf.contrib.layers.xavier_initializer())
+        b2 = tf.get_variable("b2", shape=(Config.n_classes), initializer=tf.contrib.layers.xavier_initialier())
+        h = tf.zeros(Config.hidden_size, tf.int32)
         ### END YOUR CODE
 
         with tf.variable_scope("RNN"):
